351925ad7425bf0d08a3bdc40b1e0c4a5f4e02c6
hyy20000804 <293615346@qq.com>
2025-12-03 14:37:18 +0800

    repair

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,22 +1,20 @@
-import { Header } from '@/components/header';
-import { Sidebar } from '@/components/sidebar';
+import { Header } from '@/components/header'
+import { Sidebar } from '@/components/sidebar'
 
-export default function DashboardLayout({
-  children,
+export default function DashboardLayout ({
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <div className="h-screen">
+    <div className='h-screen'>
       <Header />
-      <div className="flex h-[calc(100vh-4rem)]">
-        <div className="hidden md:flex w-64 flex-col">
+      <div className='flex h-[calc(100vh-4rem)]'>
+        <div className='hidden md:flex w-64 flex-col'>
           <Sidebar />
         </div>
-        <main className="flex-1 overflow-y-auto p-8">
-          {children}
-        </main>
+        <main className='flex-1 overflow-y-auto p-8'>{children}</main>
       </div>
     </div>
-  );
+  )
 }
